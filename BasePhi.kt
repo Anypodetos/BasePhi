4083d98964f7bc480e45fbe95f27536abb414b94
Anypodetos <75668220+Anypodetos@users.noreply.github.com>
2022-12-27 12:56:19 +0100

    License

--- BasePhi.kt
+++ BasePhi.kt
@@ -1,3 +1,22 @@
+/*
+Copyright 2022 Anypodetos (Michael Weber)
+
+BasePhi.kt is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BasePhi.kt is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BasePhi.kt. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: <https://lemizh.conlang.org/home/contact.php?about=qonvert>
+*/
+
 import java.math.BigInteger
 import java.math.BigInteger.*
 import java.util.*
